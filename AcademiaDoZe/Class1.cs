942b61fa24110fa781ab6aa31b0b624a20e3bc38
Endh3 <matheusrpites@gmail.com>
2026-08-03 17:09:54 -0300

    Adicionando novos arquivos do projéto

--- AcademiaDoZe/Class1.cs
+++ AcademiaDoZe/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace AcademiaDoZe
-{
-    public class Class1
-    {
+﻿using AcademiaDoZe.Domain.Exceptions;
+namespace AcademiaDoZe.Domain.Entities;
 
+public abstract class Entity
+{
+    public int Id { get; protected set; }
+    protected Entity(int id = 0)
+    {
+        if (id < 0) throw new Exception("ID_NEGATIVO");
+        Id = id;
     }
 }
